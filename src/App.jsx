0d60de1058834c9bb80e9506b4622b10bba07855
KRISH2507 <thekrishdeepsingh@gmail.com>
2025-04-08 09:31:25 +0530

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+// App.js
+import React from 'react';
+import './App.css';
+import DestinationContainer from './DestinationContainer'; // Import the parent component
+
 function App() {
   return (
-    <div>
-      {/* Your code here */}
+    <div className="App">
+      <h1>Top Travel Destinations</h1>
+      <DestinationContainer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
